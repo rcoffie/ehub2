3354d842fd70c161e3c611f0f441e828f4bf6a4a
Robert Coffie <rcoffie22@yahoo.com>
2020-06-25 18:37:31 +0100

    second

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+  return render(request,'pages/index.html')
+
+
+
+def about(request):
+  return render(request,'pages/about.html')
