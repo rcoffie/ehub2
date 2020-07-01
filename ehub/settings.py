9fe4d3392658a00615b533d5d681361dbcacab2c
Robert Coffie <rcoffie22@yahoo.com>
2020-07-01 16:49:23 +0100

    wenesday2

--- ehub/settings.py
+++ ehub/settings.py
@@ -76,16 +76,31 @@ WSGI_APPLICATION = 'ehub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+"""
+DATABASES = #{
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        #'ENGINE': 'django.db.backends.postgresql',
+       # 'NAME':'ehub',
+       # 'USER':'postgres',
+       # 'PASSWORD':'nana',
+        #'HOST':'localhost',
+
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+
+
+
 DATABASES = {
     'default': {
-       # 'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'uhub',
-        'USER':'postgres',
-        'PASSWORD':'nana',
-        'HOST':'localhost'
-
-       #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'ehub',
+        'USER': 'postgres',
+        'PASSWORD': 'nana',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
