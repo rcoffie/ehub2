a3729e9a8c3ee8ce0c4fddec487da4148cbffe9a
Robert Coffie <rcoffie22@yahoo.com>
2020-07-05 14:07:05 +0100

    sunday2

--- ehub/settings.py
+++ ehub/settings.py
@@ -148,3 +148,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'ehub/static')
 ]
+
+# MEDIA folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
