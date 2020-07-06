79600eb46f4926213813941606897e81075076c2
Robert Coffie <rcoffie22@yahoo.com>
2020-07-06 20:29:36 +0100

    monday2nd

--- ads/views.py
+++ ads/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,redirect 
 from .models import *
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 
 # Create your views here.
 
 def index(request):
   ads = Ads.objects.all()
-  context = {'ads':ads}
+  paginator = Paginator(ads,6)
+  page = request.GET.get('page')
+  paged_ads = paginator.get_page(page)
+  context = {'ads':paged_ads}
   return render(request,'ads/index.html',context)
