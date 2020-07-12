434984e44ca9fb7753b76957f8113ada655b535c
Robert Coffie <rcoffie22@yahoo.com>
2020-07-12 12:54:05 +0100

    sunday

--- ads/urls.py
+++ ads/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     
-   path('',views.index,name='index')
+   path('',views.index,name='ads'),
+   #path('ad/',views.ad,name='ad')
+   path('<int:ad_id>',views.ad)
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render,redirect 
+from django.shortcuts import get_object_or_404, render,redirect 
 from .models import *
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
+
 
 # Create your views here.
 
 def index(request):
-  ads = Ads.objects.all()
+  ads = Ads.objects.order_by('-date_posted').filter(published=True)
   paginator = Paginator(ads,6)
   page = request.GET.get('page')
   paged_ads = paginator.get_page(page)
   context = {'ads':paged_ads}
   return render(request,'ads/index.html',context)
+
+
+
+def ad(request,ad_id):
+  ad = get_object_or_404(Ads,pk=ad_id)
+  context = {'ad':ad,}
+  return render(request,'ads/ad.html',context)
